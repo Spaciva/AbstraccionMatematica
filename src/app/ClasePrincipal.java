8998e563f48d54a67fa1238c30877acb7e1dd200
Francisco <fa098138@gmail.com>
2025-07-29 18:29:26 -0600

    metodos y miembros ejemplo

--- src/app/ClasePrincipal.java
+++ src/app/ClasePrincipal.java
@@ -1,5 +1,8 @@
 package app;
 
 public class ClasePrincipal {
+    public static void main(String[] args) {
+
+    }
 
 }
--- src/modelo/ConjuntoEnteros.java
+++ src/modelo/ConjuntoEnteros.java
@@ -1,7 +1,45 @@
 package modelo;
-
+import java.util.ArrayList;
 public class ConjuntoEnteros {
 
+    private ArrayList<Integer> elementos;
+
+    public ConjuntoEnteros() {
+        elementos = new ArrayList<>();
+    }
+    public int getCantidad() {
+        return elementos.size();
+    }
+    public boolean pertenece(int n) {
+        return elementos.contains(n);
+    }
+
+    public boolean insertar(int n) {
+        if (!elementos.contains(n)) {
+            elementos.add(n);
+            return true;
+        }
+        return false;
+    }
+
+    public Integer extraer(int n) {
+        if (elementos.contains(n)) {
+            elementos.remove(Integer.valueOf(n));
+            return n;
+        }
+        return null;
+
+    }
+
+    public void copiarElementos(int[] destino) {
+        if (elementos.size() != destino.length) {
+            System.out.println("El arreglo destino no tiene el tamaño correcto.");
+            return;
+        }
+        for (int i = 0; i < elementos.size(); i++) {
+            destino[i] = elementos.get(i);
+        }
+    }
 
 
 }
